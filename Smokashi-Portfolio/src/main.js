d07f80ba040462f998ebfb2f76df1948d739ceec
AbdullahYounas <abdullahyounas24082005@gmail.com>
2025-04-09 20:38:06 +0500

    V0.1

--- Smokashi-Portfolio/src/main.js
+++ Smokashi-Portfolio/src/main.js
@@ -1,20 +1,25 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import getStarfield from './getStarfield.js';
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
+const fov = 90;
 const aspect = w / h;
 const near = 0.1;
-const far = 10;
+const far = 50;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enablePan = false;
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial({
   color:0xccff,
@@ -23,10 +28,14 @@ const mat = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+const stars = getStarfield();
+scene.add(stars);
+
 const light = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(light);
 function animate(t = 0){
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  controls.update();
 }
 animate();
